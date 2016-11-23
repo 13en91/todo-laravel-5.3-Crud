c6455b87b92c5c0b3d817a2df8ccb3d0e6483305
Simon Suh <simon.j.suh@gmail.com>
2016-11-22 23:01:31 -0800

    files updated

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+use Session;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::orderBy('id', 'desc')->paginate(5);
+
+        return view('index')->with('storedTasks', $tasks);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $this->validate($request, [
+                'newTaskName' => 'required|min:5|max:255',
+            ]);
+
+        $task = new Task;
+
+        $task->name = $request->newTaskName;
+
+        $task->save();
+
+        Session::flash('success', 'New task successfully added!');
+
+        return redirect()->route('tasks.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $task = Task::find($id);
+
+        return view('edit_task')->with('taskUnderEdit', $task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+                'editedTaskName' => 'required|min:5|max:255',
+            ]);
+
+        $task = Task::find($id);
+
+        $task->name = $request->editedTaskName;
+
+        $task->save();
+
+        Session::flash('success', 'Task #' . $id . ' has been successfully updated.');
+
+        return redirect()->route('tasks.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+
+        $task->delete();
+
+        Session::flash('success', 'Task #' . $id . ' has been successfully destroy.');
+
+        return redirect()->route('tasks.index');
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+
+  {{-- bootstrap css --}}
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  {{-- bootstrap js --}}
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+  <title>Todo List</title>
+</head>
+<body>
+<div class="container">
+  <div class="col-md-offset-2 col-md-8">
+    <div class="row">
+      <h1>Todo List</h1>
+    </div>
+
+    {{-- flash success message --}}
+    @if (Session::has('success'))
+      <div class="alert alert-success">
+        <strong>Success:</strong> {{ Session::get('success') }}
+      </div>
+    @endif
+
+    {{-- flash error message --}}
+    @if (count($errors) > 0) 
+      <div class="alert alert-danger">
+        <strong>Errors:</strong>
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
+
+    {{-- Add new task --}}
+    <div class="row" style='margin-top: 10px; margin-bottom: 10px;'>
+      <form action="{{ route('tasks.store') }}" method='POST'>
+      {{ csrf_field() }}
+
+        <div class="col-md-9">
+          <input type="text" name='newTaskName' class='form-control' placeholder='Add new task here'>
+        </div>
+        <div class="col-md-3">
+          <input type="submit" value='Add Task' class='btn btn-primary form-control'>
+        </div>
+      </form>
+    </div>
+
+    {{-- display stored task --}}
+    @if (count($storedTasks) > 0)
+      <div class="row">
+        <table class="table">
+          <thead>
+            <th>Task #</th>
+            <th>Name</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </thead>
+
+          <tbody>
+            @foreach($storedTasks as $storedTask)
+              <tr>
+                <th>{{ $storedTask->id }}</th>
+                <td>{{ $storedTask->name }}</td>
+                <td><a href="{{ route('tasks.edit', [''=>$storedTask->id]) }}" class='btn btn-default'>Edit</a></td>
+                <td>
+                  <form action="{{ route('tasks.destroy', [''=>$storedTask->id]) }}" method='POST'>
+                    {{ csrf_field() }}
+                    <input type="hidden" name='_method' value='DELETE'>
+
+                    <input type="submit" value='Delete' class='btn btn-danger'>
+                  </form>
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    @endif
+
+    <div class="row text-center">
+      {{ $storedTasks->links() }}
+    </div>
+
+  </div>
+</div>
+</body>
+</html>
